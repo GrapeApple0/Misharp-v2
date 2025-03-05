7185b22b3bf5500a61077536eae7b2586ea5658b
GrapeApple <84321396+GrapeApple0@users.noreply.github.com>
2025-03-05 15:37:50 +0900

    Change example program Program.cs

--- Misharp.Console/Program.cs
+++ Misharp.Console/Program.cs
@@ -10,10 +10,10 @@ public abstract class Program
     public static async Task Main()
     {
         var app = new App(Config.Server, Config.Token);
-        await using var fs = new FileStream("image.png", FileMode.Open);
-        var file = (await app.DriveApi.FilesApi.Create(fs)).Result;
+        await using var image = File.OpenRead("image.png");
+        var file = (await app.DriveApi.FilesApi.Create(image)).Result;
         var note = (await app.NotesApi.Create(text: "Test", fileIds: new List<string> { file.Id },
-            visibility: NotesApi.NotesCreatePropertiesVisibilityEnum.Specified)).Result;
+            visibility: NotesApi.NotesCreatePropertiesVisibilityEnum.Home)).Result;
         Console.WriteLine(note.ToString());
     }
 }
